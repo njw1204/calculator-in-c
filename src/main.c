8acaa68670388d16e86f39c88ee431fd5a1ade84
wonwooyang <wonwooyang@naver.com>
2018-05-30 16:29:38 +0900

    Make Ending Func

--- src/main.c
+++ src/main.c
@@ -7,12 +7,22 @@
 char expRaw[100000];
 CalcData exp[100000];
 
+void Ending();
+
 int main() {
+  Intro();
   while (1) {
     if (InputExp(expRaw) == 0) break;
-	ExpParse(expRaw, exp);
-	PrintResult(CalcExp(exp));
+	  ExpParse(expRaw, exp);
+	  PrintResult(CalcExp(exp));
   }
+	Ending();
   system("pause");
   return 0;
+}
+
+void Ending() {
+	print("\n------------------------\n");
+	printf("| End of calculation |");
+	print("\n------------------------\n");
 }
